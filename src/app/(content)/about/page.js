8ba4c89d4313c8e105a788c0e7a8d9cb4737ca06
tengyan <tytengty@cn.ibm.com>
2025-03-20 22:45:27 +0800

    fix issues

---
+++ src/app/(content)/about/page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Page() {
+  return (
+    <div className='h-screen text-3xl text-white flex items-center justify-center '>
+      About
+    </div>
+  )
+}
