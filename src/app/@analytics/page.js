fe40ec0a101a06d99cc147c3f072e35a78d1fc3d
tengyan <tytengty@cn.ibm.com>
2025-03-27 10:43:47 +0800

    add paralles-routes

---
+++ src/app/@analytics/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='flex m-6 justify-center text-white **:rounded-lg items-center h-60 flex-1 bg-teal-400'>
+      @Analytics
+    </div>
+  )
+}
---
+++ src/app/@teams/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='flex justify-center m-6 text-white rounded-lg items-center h-60 flex-1 bg-purple-400'>
+      @teams
+    </div>
+  )
+}
